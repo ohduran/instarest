0238141a4de823a6bc3f2d631aa806f41de1a856
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-17 23:15:13 +0100

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,7 +72,6 @@ LOCAL_APPS = [
     'instarest.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
     'common',
-    'likes',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
