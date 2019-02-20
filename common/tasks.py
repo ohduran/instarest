22e10860a402a2b280099aa2308b854bb9010ca9
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-20 21:14:00 +0100

    simplify requirements

--- common/tasks.py
+++ common/tasks.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
+from instapy.util import smart_run
 
 from .utils import get_session
 
@@ -9,5 +10,6 @@ from .utils import get_session
 @shared_task()
 def check_bot(username, password):
     """Check whether the bot is usable or not"""
-    bot = get_session(username, password)
+    session = get_session(username, password)
+    session.login()
     return True
--- common/utils.py
+++ common/utils.py
@@ -6,6 +6,4 @@ def get_session(username, password):
                       password=password,
                       selenium_local_session=False)
     session.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
-    session.login()
-    # session.end()
     return session
