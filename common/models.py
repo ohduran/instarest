cc74d1eab057638a0c61da9ef836a056e48ba47e
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-22 17:41:46 +0100

    start over

--- common/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Bot(models.Model):
-
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
--- common/utils.py
+++
@@ -1,9 +0,0 @@
-from instapy import InstaPy
-
-
-def get_session(username, password):
-    session = InstaPy(username=username,
-                      password=password,
-                      selenium_local_session=False)
-    session.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
-    return session
