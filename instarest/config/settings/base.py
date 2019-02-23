82cc9ffe04c0c5b5996aa1593753a210258b5da9
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-23 10:30:51 +0100

    bots in settings

--- instarest/config/settings/base.py
+++ instarest/config/settings/base.py
@@ -71,6 +71,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'instarest.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
+    'bots',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
