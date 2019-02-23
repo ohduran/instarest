01470773441bcb7fcd2335cebfd6ba86cd906940
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-23 18:57:27 +0100

    verified renaming complete

--- instarest/bots/tests/test_views.py
+++ instarest/bots/tests/test_views.py
@@ -1,5 +1,4 @@
 import pytest
-from model_mommy import mommy
 from rest_framework import status
 
 from ..models import Bot
@@ -8,18 +7,16 @@ from ..models import Bot
 @pytest.mark.django_db
 class TestBotViewSet:
 
-    def test_Post_GivenUsernameAndPassword_Returns201(self, tp):
+    def test_Post_GivenUsernameAndPassword_SlugIsUsername(self, tp):
         data = {'username': 'test', 'password': 'testpassword'}
         response = tp.post('bot-list', data=data, extra={'format': 'json'})
 
-        expected_json = {'id': 1, 'slug': 'test', 'username': 'test', 'is_verified': False}
+        expected_json = {'id': 1, 'username': 'test', 'is_verified': False}
         assert expected_json == response.json()
         assert status.HTTP_201_CREATED == response.status_code
 
-    # def test_Retrieve_GivenASlug_ReturnsTheCorrespondingInstance(self, tp):
-    #     bot = mommy.make(Bot, username='test', slug='test', password='testpassword')
-    #     url = tp.reverse('bot-detail', args=['test'])
-    #
-    #     response = tp.get(url)
-    #     assert status.HTTP_200_OK == response.status_code
-    #     assert response.json()
+        assert 1 == Bot.objects.count()
+
+        bot = Bot.objects.first()
+
+        assert 'test' == bot.slug
