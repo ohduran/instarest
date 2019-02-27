acc7419da91d946cfca5241b3b4bb0301f0d3e5e
Alvaro Duran <alvaro.duranb@gmail.com>
2019-02-27 19:12:12 +0100

    wip

--- instarest/bots/selenium.py
+++ instarest/bots/selenium.py
@@ -2,13 +2,11 @@ from instapy import InstaPy
 
 
 def get_session(bot):
-    try:
-        session = InstaPy(username=bot.username,
-                          password=bot.password,
-                          selenium_local_session=False)
-        session.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
-        session.login()
-        session.end()
-        return True
-    except Exception:
-        return False
+    session = InstaPy(username=bot.username,
+                      password=bot.password,
+                      selenium_local_session=False)
+    session.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
+    session.login()
+    session.end()
+    # session.aborting turns True iff login failed.
+    return not session.aborting
