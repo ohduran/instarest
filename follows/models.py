8b6532915cdb02eafe6d153ea97b372c53fc95a2
Alvaro Duran <alvaro.duranb@gmail.com>
2019-03-01 16:25:26 +0100

    create follow app

---
+++ follows/models.py
@@ -0,0 +1,24 @@
+from common.behaviors import Permalinkable, Timestampable
+from django.db import models
+
+
+class FollowQueryset(models.QuerySet):
+    pass
+
+
+class FollowManager(models.Manager):
+    queryset_class = FollowQueryset
+    use_for_related_fields = True
+
+    def completed(self, **kwargs):
+        return self.filter(is_complete=True, **kwargs)
+
+
+class Follow(Timestampable, Permalinkable, models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    bot = models.ForeignKey('bots.Bot', on_delete=models.CASCADE)
+
+    objects = FollowManager()
+
+    def __str__(self):
+        return '@{}'.format(self.username)
