d2cc06666d6eedfb06bc0b94f7865891f6349605
Alvaro Duran <alvaro.duranb@gmail.com>
2019-03-01 20:01:06 +0100

    test timestampable behavior

--- common/tests/test_behaviors.py
+++ common/tests/test_behaviors.py
@@ -13,10 +13,20 @@ class BehaviorTestMixin:
 
 class TimestampableTests(BehaviorTestMixin):
 
-    def test_timestampable_object(self):
+    def test_created_date(self):
 
         now = timezone.now()
         with freeze_time(now):
             obj = self.create_instance()
 
         assert now == obj.created_date
+
+    def test_modified_date(self):
+
+        obj = self.create_instance()
+
+        now = timezone.now()
+        with freeze_time(now):
+            obj.save()
+
+        assert now == obj.modified_date
